ae354cbefc5acd1d988684538622c0eaa963b63b
terry <921588402@qq.com>
2021-01-18 20:05:52 +0800

    Jedis_API+文章访问量排行榜

--- src/main/java/com/terry/redis/JedisUtil.java
+++ src/main/java/com/terry/redis/JedisUtil.java
@@ -3,31 +3,9 @@ package com.terry.redis;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
-import redis.clients.jedis.Tuple;
 
-import javax.xml.soap.Text;
-import java.lang.reflect.InvocationTargetException;
-import java.util.Set;
-import java.util.zip.DeflaterOutputStream;
 
-public class RedisUtil {
-    public static void main(String[] args) {
-        Jedis jedis = RedisUtil.getJedisFromPool();
-
-        Set<String> keyset = jedis.keys("*");
-        for (String key : keyset) {
-            System.out.println(key);
-        }
-
-        Set<Tuple> top3 = jedis.zrevrangeWithScores("article:topn", 0, 2);
-        for (Tuple tuple : top3) {
-            System.out.println(tuple.getElement()+":"+tuple.getScore());
-        }
-
-        System.out.println(jedis.ping());
-        jedis.close();
-    }
-
+public class JedisUtil {
     private static JedisPool jedisPool = null;
 
     public static Jedis getJedisFromPool(){
@@ -47,4 +25,5 @@ public class RedisUtil {
             return jedisPool.getResource();
         }
     }
+
 }
